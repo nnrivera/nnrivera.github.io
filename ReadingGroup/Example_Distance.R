91e4ecb5781e9a4e9ce13760b3bd733f36dc3aa5
nnrivera <69904812+nnrivera@users.noreply.github.com>
2023-08-22 22:37:56 -0400

    HDS1

---
+++ ReadingGroup/Example_Distance.R
@@ -0,0 +1,29 @@
+rm(list = ls())
+#Example 1.2.3 of HDS by Martin J. Wainwright.
+
+#number of data points
+n = 1000
+
+#we use monte-carlo method to compute the expected distance of a random point in $[0,1]^d$ towards a set of n random points in $[0,1]^d$
+
+#We use different distances d, in the vector ds
+ds = floor(seq(1,floor(sqrt(n)), length=20))
+distances_ds =rep(0,length(ds))
+M = 1000
+
+for(j in 1:length(ds))
+{
+  d = ds[j]
+  dist = rep(0,M)
+  for (i in 1:M)
+  {
+    X = matrix(runif(n*d), ncol = d, nrow = n) #set of n random points in $[0,1]^d$
+    Xp = runif(d) #random point in [0,1]^d
+    dist[i] <- min(apply(abs(t(t(X)-Xp)), 1, max, na.rm=TRUE)) #\ell_{\infty} distance
+  }
+  distances_ds[j] = mean(dist)
+}
+
+plot(ds,distances_ds, xlab = "dimension", ylab = "expected distance")
+
+           
---
+++ ReadingGroup/Example_covariance.R
@@ -0,0 +1,31 @@
+rm(list = ls())
+library("mvtnorm")
+
+#Example 1.2.2 and 1.3.2 of HDS by Martin J. Wainwright.
+
+#Number of data points and dimension
+n = 10000
+d = 200
+
+#Data X ~ N(0, I_d)
+X = rmvnorm(n, mean = rep(0, d), sigma = diag(d))
+
+#estimated covariance matrix
+Sigmahat = (t(X)%*%X)/n
+
+
+#If d<<n, then eigenValues of Sigmahat are close to 1 (since Sigma = I_d)
+#Otherwise, their distribution is close to the Marčenko–Pastur law
+eigenValues <- eigen(Sigmahat)$values
+hist(eigenValues)
+summary(eigenValues)
+
+
+####Solution: Hard-threshold as proposed in 1.3.2
+#The idea works in any sparse covariance matrix, not only the identity
+
+newSigmahat = Sigmahat*(abs(Sigmahat)>sqrt(2*log(d)/n))
+new_eigenValues <- eigen(newSigmahat)$values
+#they are very concentrated around 1, as it should for Sigma = Id
+hist(new_eigenValues)
+summary(new_eigenValues)
